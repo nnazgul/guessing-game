2e4879d5b052678b1ad82e08edf6119ad630fd21
nnazgul <nazgul.nurmatova.ch@gmail.com>
2023-02-24 00:06:23 +0600

    feat: complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,31 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = null;
+        this.max = null;
+        this.medium = null;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        let medium = this.max - Math.floor((this.max - this.min) / 2);
+        this.medium = medium
+        return this.medium;
     }
 
     lower() {
-
+        this.max = this.medium;
+        let game = new GuessingGame;
+        game.guess();
     }
 
     greater() {
-
+        this.min = this.medium;
+        let game = new GuessingGame;
+        game.guess();
     }
 }
 
